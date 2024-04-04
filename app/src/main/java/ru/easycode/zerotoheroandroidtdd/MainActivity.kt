146d87bb192f4436bc0f1dd29df94ef058e8d710
MaJlblLLlaH9 <majlbllllah9@gmail.com>
2024-04-05 00:56:04 +0300

    text 12 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,40 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val count = Count.Base(2, 4)
+    private var state: UiState = UiState.Base("0")
+
+    lateinit var textView: TextView
+    lateinit var button: Button
+    lateinit var linearLayout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.countTextView)
+        button = findViewById(R.id.incrementButton)
+        linearLayout = findViewById(R.id.rootLayout)
+
+
+        button.setOnClickListener {
+            state = count.increment(textView.text.toString())
+            state.apply(textView, button)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("key", state)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        state = savedInstanceState.getSerializable("key") as UiState
+        state.apply(textView, button)
     }
 }
